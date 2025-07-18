c7e137997670e80f8336b2fb648bba2b768e7048
madasgaskar <s4tr10.d1m4s@gmail.com>
2025-07-18 22:52:26 +0700

    lomba saya4

--- resources/views/pdf/bukti_pembayaran.blade.php
+++ resources/views/pdf/bukti_pembayaran.blade.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Bukti Pembayaran</title>
     <style>
-        body { font-family: sans-serif; }
-        .header { margin-bottom: 20px; }
-        .qr { margin-top: 20px; }
+        body {
+            font-family: sans-serif;
+        }
+
+        .header {
+            margin-bottom: 20px;
+        }
+
+        .qr {
+            margin-top: 20px;
+        }
     </style>
 </head>
+
 <body>
     <h2>Bukti Pembayaran</h2>
     <p>Nomor Transaksi: {{ $transaksi->order_id }}</p>
@@ -15,10 +25,19 @@
     <p>Tanggal: {{ $transaksi->tanggal_transaksi }}</p>
     <p>Metode Pembayaran: {{ $transaksi->metode_pembayaran }}</p>
     @if ($qrcodePath)
-    <img src="file://{{ $qrcodePath }}" width="150" height="150" alt="QR Code">
+        @if ($qrcodePath)
+            @php
+                $qrData = base64_encode(file_get_contents($qrcodePath));
+                $qrMime = mime_content_type($qrcodePath); // biasanya image/png
+            @endphp
+            <img src="data:{{ $qrMime }};base64,{{ $qrData }}" width="150" height="150" alt="QR Code">
+        @endif
+
+        {{-- <img src="file://{{ $qrcodePath }}" width="150" height="150" alt="QR Code"> --}}
     @endif
 
 
 
 </body>
+
 </html>
