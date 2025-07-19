3a31e5916e1bc25614b1111ca6ec7b0741ab6182
madasgaskar <s4tr10.d1m4s@gmail.com>
2025-07-19 16:36:15 +0700

    scaan qr

--- resources/views/bendahara/scan.blade.php
+++ resources/views/bendahara/scan.blade.php
@@ -145,18 +145,19 @@
                     //     console.log(item);
                     // }, 2000);
                     table.clear().draw();
-                    data.data.forEach((item, index) => {
+                    data.data.forEach((item) => {
                         const date = new Date(item.updated_at);
                         const formattedDate = date.toLocaleString();
 
-                        table.row.add([
-                            item.user.name,
-                            item.transaksi?.pemesanans?.burung?.jenis_burung?.nama ?? '-',
-                            item.transaksi?.pemesanans?.burung?.kelas?.nama ?? '-',
-                            formattedDate
-                        ]).draw();
+                        item.transaksi?.pemesanans?.forEach(pemesanan => {
+                            table.row.add([
+                                item.user.name,
+                                pemesanan.burung?.jenis_burung?.nama ?? '-',
+                                pemesanan.burung?.kelas?.nama ?? '-',
+                                formattedDate
+                            ]).draw();
+                        });
                     });
-
                 } else {
                     var notyf = new Notyf();
                     notyf.error('QRCode tidak ditemukan!');
