928a4f59153feb70385edf8a8a7f0daf2a08b46c
madasgaskar <s4tr10.d1m4s@gmail.com>
2025-08-05 15:26:33 +0700

    payment production2

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'serverKey'     => env('MIDTRANS_SERVER_KEY'),
-    'clientKey'     => env('MIDTRANS_CLIENT_KEY'),
-    'snapUrl'       => env('MIDTRANS_SNAP_URL'),
-    'isProduction'  => env('MIDTRANS_IS_PRODUCTION', false),
-    'isSanitized'   => env('MIDTRANS_IS_SANITIZED', true),
-    'is3ds'         => env('MIDTRANS_IS_3DS', true),
+    'serverKey' => env('MIDTRANS_SERVER_KEY'),
+    'clientKey' => env('MIDTRANS_CLIENT_KEY'),
+    'snapUrl' => env('MIDTRANS_SNAP_URL'),
+    'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
+    'isSanitized' => env('MIDTRANS_IS_SANITIZED', true),
+    'is3ds' => env('MIDTRANS_IS_3DS', true),
 ];
